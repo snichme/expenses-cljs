a56fb88d60fe11b823f39be416122d1c7c206ae6
Magnus Landerblom <magnus@landerblom.se>
2015-06-06 15:08:11 +0200

    ui for switching lang

--- src/cljs/expenses/core.cljs
+++ src/cljs/expenses/core.cljs
@@ -7,8 +7,13 @@
    [expenses.calc :refer [sum-expenses mean-expense pay]]
    [expenses.actions :as actions]
    [expenses.components.expenses :refer [expenses expenses-plain]]
-   [expenses.components.payments :refer [payments]]))
+   [expenses.components.payments :refer [payments]])
+  (:import
+   [goog.i18n NumberFormatSymbols]
+   [goog.i18n NumberFormatSymbols_sv_SE]
+   [goog.i18n NumberFormatSymbols_en_US]))
 
+(set! NumberFormatSymbols NumberFormatSymbols_sv_SE)
 
 (defonce app-state (atom {:items [
                                   {:name "Mange" :amount 100}
@@ -17,6 +22,28 @@
                                   {:name "Doris" :amount 10}
                                   ]}))
 
+(defn set-lang [lang]
+  (println lang)
+  (case lang
+    :se (set! NumberFormatSymbols NumberFormatSymbols_sv_SE)
+    :en (set! NumberFormatSymbols NumberFormatSymbols_en_US)))
+
+(defn language-selector [app owner]
+  (om/component
+   (dom/ul nil
+           (dom/li nil
+                   (dom/a #js {
+                            :href "#"
+                            :onClick (fn [_] (set-lang :se))} "Svenska"))
+           (dom/li nil
+                   (dom/a #js {
+                            :href "#"
+                            :onClick (fn [_] (set-lang :en))} "Engelska"))
+           )))
+(defn header [app owner]
+  (om/component
+   (dom/div #js{:className "header"}
+            (om/build language-selector app))))
 
 (defn app [data owner]
   (reify
@@ -30,6 +57,7 @@
     om/IRender
     (render [_]
       (dom/div #js {:className "app"}
+               (om/build header data)
                (expenses (:items data))
                (expenses-plain (:items data))
                (payments (:items data))))))
